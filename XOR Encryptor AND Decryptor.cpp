413f08d4b68f3c02a191d41e47455a7870563f29
SKhizarAhmed <160646646+SKhizarAhmed@users.noreply.github.com>
2026-06-03 00:24:24 +0500

    Update XOR Encryptor AND Decryptor.cpp

--- XOR Encryptor AND Decryptor.cpp
+++ XOR Encryptor AND Decryptor.cpp
@@ -1,4 +1,3 @@
-/*
 #include <iostream>
 #include <conio.h>
 #include <string.h>
@@ -45,10 +44,22 @@ int main() {
 	return 0;
 }
 
-*/
 
 /*The purpose of k%3 is that as the iteration updates we will divide it by 3 and the remainder
 		* which will in the range of 0,1,2 (less than 3) will serve as index of key which exactly have 3
 		* elements
+		so randomized XOR Operation will be performed with a repetition structure of 3,5,4 
+		The operator of XOR is ^
+		Also to understand XOR operation you might need a familarity with binary like if key = 5
+		and we need to XOR the letter 'A' (Note: 'a' and 'A' are different due to difference in ASCII code)
+		ASCII code for A is 65
+		XOR returns 1 whenever the inputs are different and returns 0 when inputs are same(i.e 0 if A is 1 and B is 1) and (1 if A is 0 or B is 1/ vice versa)
+		IN BINARY: 01000001
+		5 in BINARY : 00000101
+		Performing XOR: A ^ key
+		01000001 ^ 5 = 01000001 ^ 00000101 = 01000100 = 68 
+		68 in ASCII is D
+		so with changing key the XOR is different not with the same gap
+		The decryption logic is also simply if you XOR 'D' with the same key 5 it will return 'A'
 		*/
 
